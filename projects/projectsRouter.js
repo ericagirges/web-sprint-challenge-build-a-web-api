56488b924ded35658cd740cbaee74302cac3d090
Erica Girges <ericafiorenza@gmail.com>
2020-10-02 09:32:18 -0700

    projects PUT endpoint working

--- projects/projectsRouter.js
+++ projects/projectsRouter.js
@@ -37,23 +37,41 @@ router.delete("/:id", (req, res) => {
 });
 
 function validateProjectContents(req, res, next) {
-    if(!req.body.description || !req.body.name) {
-        res.status(400).json({ message: "Please provide a project name and descripton."})
-    } else {
-        next()
-    }
+  if (!req.body.description || !req.body.name) {
+    res
+      .status(400)
+      .json({ message: "Please provide a project name and descripton." });
+  } else {
+    next();
+  }
 }
 
 // POST project
 router.post("/", validateProjectContents, (req, res) => {
-    Projects.insert(req.body)
+  Projects.insert(req.body)
     .then((project) => {
-        res.status(201).json(project)
+      res.status(201).json(project);
     })
     .catch((error) => {
-        console.log("ERROR: ", error);
-        res.status(500).json({ message: "Error adding a project."})
+      console.log("ERROR: ", error);
+      res.status(500).json({ message: "Error adding a project." });
+    });
+});
+
+// PUT project by id
+router.put("/:id", validateProjectContents, (req, res) => {
+  Projects.update(req.params.id, req.body)
+    .then((project) => {
+      if (project) {
+        res.status(200).json(project);
+      } else {
+        res.status(404).json({ message: "The project could not be found." });
+      }
     })
-})
+    .catch((error) => {
+      console.log("ERROR: ", error);
+      res.status(500).json({ message: "Error updating project." });
+    });
+});
 
 module.exports = router;
