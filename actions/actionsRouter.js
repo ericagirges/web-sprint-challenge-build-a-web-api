c4367a162d35e2ea4379a140d6f21bef8cb1a611
Erica Girges <ericafiorenza@gmail.com>
2020-10-02 10:37:15 -0700

    progress on actionsRouter

--- actions/actionsRouter.js
+++ actions/actionsRouter.js
@@ -1,18 +1,26 @@
 const express = require("express");
 
 const Actions = require("../data/helpers/actionModel");
+const byProjectId = require("../data/helpers/projectModel");
 
 const router = express.Router();
 
 router.get("/:id/actions", (req, res) => {
-    Actions.get(req.query)
+  byProjectId
+    .get(req.params.projectId)
     .then((actions) => {
+      if (projectId) {
         res.status(200).json(actions);
+      } else {
+        res
+          .status(404)
+          .json({ message: "Project actions could not be found." });
+      }
     })
     .catch((error) => {
-        console.log("ERROR: ", error);
-        res.status(500).json({ message: "Error retrieving actions."})
-    })
-})
+      console.log("ERROR: ", error);
+      res.status(500).json({ message: "Error retrieving actions." });
+    });
+});
 
 module.exports = router;
