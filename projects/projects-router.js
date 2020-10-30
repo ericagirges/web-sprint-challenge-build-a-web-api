50943f779388d9ce24b03e72db72bd321a113b26
Erica Girges <ericafiorenza@gmail.com>
2020-10-30 09:16:27 -0700

    projects router completed

--- projects/projects-router.js
+++ projects/projects-router.js
@@ -17,31 +17,72 @@ router.get("/", (req,res) => {
 // GET project by projectId
 router.get("/:projectId", (req, res) => {
     Projects.get(req.params.id)
-    .then(project => {
-        if(!project) {
-            res.status(404).json({ message: "Project not found." });
+      .then((project) => {
+        if (project) {
+          res.status(200).json(project);
         } else {
-            res.status(200).json({ project });
+          res.status(404).json({ message: "Project not found." });
         }
-    }) .catch(error => {
-        res.status(500).json({ message: "Error retriveing the project." });
-    });
-});
+      })
+      .catch((error) => {
+        console.log("ERROR: ", error);
+        res.status(500).json({ message: "Error retrieving project." });
+      });
+  });
+
+
+// middleware to validate incoming project data
+function validateProjectContents(req, res, next) {
+    if (!req.body.description || !req.body.name) {
+      res
+        .status(400)
+        .json({ message: "Please provide a project name and descripton." });
+    } else {
+      next();
+    }
+  }
 
 // POST add a new project
-router.post("/", (req, res) => {
+router.post("/", validateProjectContents, (req, res) => {
     Projects.insert({
         name: req.body.name,
         description: req.body.description
     }) .then(newProject => {
-        if(!newProject.name || !newProject.description){
-            res.status(400).json({ message: "Please provide a name and description for the project." });
-        } else {
-            res.status(201).json({ message: "Project has been successfully added." });
-        }
+        res.status(201).json({ message: "Project has been successfully added." });
     }) .catch(error => {
         res.status(500).json({ error: "Could not add a new project at this time." });
     });
 });
+
+// PUT update/edit a project
+router.put("/:projectId", validateProjectContents, (req, res) => {
+    Projects.update(req.params.id, req.body) 
+    .then(project => {
+        const id = req.params.id
+        if(id !== project.id){
+            res.status(404).json({ message: "Could not find a project that matches the given id." })
+        } else {
+            res.status(200).json({ message: "Project has been successfully updated." })
+        }
+    }) .catch(error => {[
+        res.status(500).json({ error: "Could not update the project at this time." })
+    ]})
+});
+
+// DELETE project by id
+router.delete("/:id", (req, res) => {
+    Projects.remove(req.params.id)
+      .then((count) => {
+        if (count > 0) {
+          res.status(200).json({ message: "The project has been deleted." });
+        } else {
+          res.status(404).json({ message: "The project cold not be found." });
+        }
+      })
+      .catch((error) => {
+        console.log("ERROR: ", error);
+        res.status(500).json({ message: "Error deleting the project." });
+      });
+  });
 
 module.exports = router;
